7e758fbd94c946bcf0dd6df55e243129fb5fec03
Daniel Kulp <dan@kulp.com>
2023-08-13 12:32:39 -0400

    Fix login workflow, add userinformation to screen

--- config.php
+++ config.php
@@ -12,16 +12,28 @@ function CheckStatus() {
         async: false,
         success: function (data) {
             if (data['status'] == "Connected") {
+                var html = "<div><b>" + data["name"] + "</b><br>";
+                html += data["email"] + "<br><br>";
+                html += "Subscription:<br>";
+                html += data["maxFPP"] + " FPP Instances<br>";
+                html += data["maxKulp"] + " KulpLights Controllers<br></div>";
+                $("#userInfoDiv").html(html);
+                $("#userInfoDiv").show();
                 $("#loginDiv").hide();
                 $("#connectedDiv").show();
+                $("#notRunningDiv").hide();
             } else {
                 $("#loginDiv").show();
+                $("#userInfoDiv").hide();
                 $("#connectedDiv").hide();
+                $("#notRunningDiv").hide();
             }
         },
         error: function(data) {
             $("#connectedDiv").hide();
-            $("#loginDiv").show();
+            $("#userInfoDiv").hide();
+            $("#loginDiv").hide();
+            $("#notRunningDiv").show();
         }
     });
 }
@@ -87,8 +99,14 @@ $(document).ready(function() {CheckStatus();});
 <div class="row"><div class="printSettingLabelCol description col-1">Password:</div><div class="col-1"><input type='password' id='klpassword'></div></div>
 <div class="row"><div class="col-1"></div><div class="col-1"><input type='button' class='buttons buttons-rounded' value="Login" onclick="LoginToKulpLights()"></div></div>
 </div>
-<div class="container-fluid settingsTable settingsGroupTable" id="connectedDiv">
-FPP Remote Monitoring Connected
+<div class="container-fluid" id="connectedDiv">
+FPP Remote Monitoring Connected<br>
+<div class=" row">
+<div class="backdrop col-auto" id="userInfoDiv"></div>
+</div>
+</div>
+<div class="container-fluid settingsTable settingsGroupTable" id="notRunningDiv">
+FPP Remote Monitoring Plugin Not Running.  Restart FPPD to enable.
 </div>
 <br>
 <div class="container-fluid settingsTable settingsGroupTable">    
